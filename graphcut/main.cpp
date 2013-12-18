15f3e611d7c4908f392d38ead4ae33319ed6b058
Carsten Haubold <carstenhaubold@googlemail.com>
2013-12-18 15:16:35 +0100

    small bugfixes, created typedef for graph type

--- graphcut/main.cpp
+++ graphcut/main.cpp
@@ -19,6 +19,8 @@
 #include <lemon/list_graph.h>
 #include <lemon/preflow.h>
 
+typedef lemon::ListGraph Graph;
+
 class ImageGraph {
 public:
     typedef vigra::MultiArray<2, vigra::UInt8> ImageArray;
@@ -38,15 +40,15 @@ protected:
                                           unsigned int x1,
                                           unsigned int y1,
                                           unsigned int width,
-                                          lemon::ListGraph::Node& a,
-                                          std::vector<lemon::ListGraph::Node>& nodes);
+                                          Graph::Node& a,
+                                          std::vector<Graph::Node>& nodes);
 
 private:
     ImageArray _imageArray;
 
-    lemon::ListGraph _graph;
-    lemon::ListGraph::EdgeMap<int> _costs;
-    lemon::ListGraph::NodeMap<Coordinate> _coordinates;
+    Graph _graph;
+    Graph::EdgeMap<int> _costs;
+    Graph::NodeMap<Coordinate> _coordinates;
 };
 
 ImageGraph::ImageGraph(const std::string &filename):
@@ -89,17 +91,19 @@ void ImageGraph::createEdgeToNodeWithIndex(unsigned int x0,
                                            unsigned int x1,
                                            unsigned int y1,
                                            unsigned int width,
-                                           lemon::ListGraph::Node& a,
-                                           std::vector<lemon::ListGraph::Node>& nodes)
+                                           Graph::Node& a,
+                                           std::vector<Graph::Node>& nodes)
 {
-    lemon::ListGraph::Node& b = nodes[y0 * width + x0];
-    lemon::ListGraph::Edge e = _graph.addEdge(a, b);
+    Graph::Node& b = nodes[y0 * width + x0];
+    Graph::Edge e = _graph.addEdge(a, b);
 
     // compute gradient magnitude
     int gradientMagnitude = _imageArray(x0, y0) - _imageArray(x1, y1);
     gradientMagnitude *= gradientMagnitude;
 
-    _costs[e] = gradientMagnitude;
+
+    std::cout << "Setting edge weight: " << gradientMagnitude << std::endl;
+    _costs[e] = 65025 - gradientMagnitude;
 }
 
 void ImageGraph::buildGraph()
@@ -110,9 +114,8 @@ void ImageGraph::buildGraph()
 
     // create nodes
     std::cout << "Generating graph nodes..." << std::endl;
-    std::vector<lemon::ListGraph::Node> nodes(width*height);
+    std::vector<Graph::Node> nodes(width*height);
     std::generate(nodes.begin(), nodes.end(), [&]() { return _graph.addNode(); });
-
 
     // fill the coordinate map
     std::cout << "Filling coordinate map..." << std::endl;
@@ -120,7 +123,8 @@ void ImageGraph::buildGraph()
     {
         for(unsigned int x = 0; x < width; x++)
         {
-            _coordinates[nodes[y * height + x]] = std::make_pair(x, y);
+            _coordinates[nodes[y * width + x]] = std::make_pair(x, y);
+            //std::cout << "Node (" << x << ", " << y << "): ID=" << _graph.id(nodes[y * width + x]) << std::endl;
         }
     }
 
@@ -130,7 +134,7 @@ void ImageGraph::buildGraph()
     {
         for(unsigned int x = 0; x < width; x++)
         {
-            lemon::ListGraph::Node& a = nodes[y * width + x];
+            Graph::Node& a = nodes[y * width + x];
 
             // for all but the last row insert edge to the next node in y
             if(y < height - 1)
@@ -150,28 +154,31 @@ void ImageGraph::buildGraph()
 ImageGraph::ImageArray ImageGraph::runMinCut(Coordinate source, Coordinate sink)
 {
     // find source and sink nodes
-    lemon::ListGraph::Node sourceNode = _graph.nodeFromId(source.second * _imageArray.shape(1) + source.first);
-    lemon::ListGraph::Node sinkNode = _graph.nodeFromId(sink.second * _imageArray.shape(1) + sink.first);
+    Graph::Node sourceNode = _graph.nodeFromId(source.second * _imageArray.shape(0) + source.first);
+    Graph::Node sinkNode = _graph.nodeFromId(sink.second * _imageArray.shape(0) + sink.first);
 
     // perform min-cut / max-flow
     std::cout << "Running Min-Cut..." << std::endl;
-    lemon::Preflow< lemon::ListGraph, lemon::ListGraph::EdgeMap<int> > preflow(_graph, _costs, sourceNode, sinkNode);
+    lemon::Preflow< Graph, Graph::EdgeMap<int> > preflow(_graph, _costs, sourceNode, sinkNode);
     preflow.init();
     preflow.runMinCut();
 
     // extract nodes on the cut
     std::cout << "Extracting results..." << std::endl;
-    lemon::ListGraph::NodeMap<bool> minCut(_graph);
-    preflow.minCutMap(minCut);
+//    Graph::NodeMap<bool> minCut(_graph);
+//    preflow.minCutMap(minCut);
 
     // create vigra image of the cut
     ImageArray cutImage(_imageArray.shape());
     cutImage = 0;
-    for(lemon::ListGraph::NodeMap<bool>::MapIt it(minCut); it != lemon::INVALID; ++it)
+
+    for (Graph::NodeIt n(_graph); n != lemon::INVALID; ++n)
     {
-        lemon::ListGraph::Node current = it;
-        Coordinate c = _coordinates[current];
-        cutImage(c.first, c.second) = 255;
+        if(preflow.minCut(n))
+        {
+            Coordinate c = _coordinates[n];
+            cutImage(c.first, c.second) = 255;
+        }
     }
 
     return cutImage;
